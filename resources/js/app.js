49f84e37401bb18e7ae05a63e2761f235972b25c
Igor-de-Almeida <varelaigor8@gmail.com>
2026-06-24 21:30:57 +0200

    Teste dos imports

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,14 @@
 console.log('Carregando o Ficheiro app.js');
 import { Calendar } from '@fullcalendar/core';
+console.log('Passou do import 1');
 import dayGridPlugin from '@fullcalendar/daygrid';
+console.log('Passou do import 2');
 import interactionPlugin from '@fullcalendar/interaction';
+console.log('Passou do import 3');
 import Echo from 'laravel-echo';
+console.log('Passou do import 4');
 import Pusher from 'pusher-js';
+console.log('Passou do import 5');
 
 window.Pusher = Pusher;
 
