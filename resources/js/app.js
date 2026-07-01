5cef36e633219af6dee8430ac9add924da36a55c
Igor-de-Almeida <varelaigor8@gmail.com>
2026-07-01 17:13:11 +0200

    Fixing Service Worker webpushern

--- resources/js/app.js
+++ resources/js/app.js
@@ -45,7 +45,7 @@ if ('serviceWorker' in navigator) {
 
         try {
 
-            const registration = await navigator.serviceWorker.register('/service-worker.js');
+            const registration = await navigator.serviceWorker.register('/webpushr-sw.js');
 
             let subscription = await registration.pushManager.getSubscription();
             
